72f2360fbe3499457c8a311bbb19f19db932db40
kentah <kenthow@gmail.com>
2015-10-09 19:26:01 -0500

    Final commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,21 @@
 'use strict';
 
-var IndexModel = require('../models/index');
+var Book = require('../models/bookModel');
 
 
-module.exports = function (router) {
-
-    var model = new IndexModel();
-
-    router.get('/', function (req, res) {
-        
-        
-        res.render('index', model);
-        
-        
-    });
-
+module.exports = function (router) { 
+  router.get('/', function (req, res) {
+    Book.find({}, function(err, books){
+      if(err){
+    	console.log(err);
+      }
+      books.forEach(function(book){
+    	book.truncText = book.truncText(50);
+      });
+      var model = {
+    	books: books
+      }
+      res.render('index', model);  
+    });                          
+  });
 };
